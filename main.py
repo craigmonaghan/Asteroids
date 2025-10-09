424a541f2988822c5b0fa1671deb5dd1cb29ae7e
craigmonaghan <craigmonaghan.op@gmail.com>
2025-10-09 19:13:33 +0100

    player movement

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pygame
 from constants import *
+from player import Player
+
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
@@ -14,8 +21,11 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
+        
 
 
 
